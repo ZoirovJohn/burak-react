76f5482b3e282aaf27f88f8e783401c52fd1f78d
Thomas <zoirovjohn@gmail.com>
2025-02-06 11:19:47 +0900

    feat: modify homePage screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -19,7 +19,6 @@ import HelpPage from "./screens/helpPage";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
-import Test from "./screens/Test";
 
 function App() {
   const location = useLocation();
@@ -41,8 +40,7 @@ function App() {
           <HelpPage />
         </Route>
         <Route path="/">
-          <Test />
-          {/* <HomePage /> */}
+          <HomePage />
         </Route>
       </Switch>
       <Footer />
