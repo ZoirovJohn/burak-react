3e71606e77be37541fe4d3c4486190525d903e0d
Thomas <zoirovjohn@gmail.com>
2025-02-13 10:40:01 +0900

    feat: develop productsPage slice and selector

--- src/lib/types/screen.ts
+++ src/lib/types/screen.ts
@@ -4,6 +4,7 @@ import { Product } from "./product";
 /** REACT APP STATE **/
 export interface AppRootState {
   homePage: HomePageState;
+  productsPage: ProductsPageState;
 }
 
 /** HOMEPAGE **/
@@ -14,4 +15,10 @@ export interface HomePageState {
 }
 
 /** PRODUCTS PAGE **/
+export interface ProductsPageState {
+  restaurant: Member | null;
+  chosenProduct: Product | null;
+  products: Product[];
+}
+
 /** ORDERS PAGE **/
