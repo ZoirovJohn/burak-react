02357e7cd84c866203226840793062bfa02faf9d
Thomas <zoirovjohn@gmail.com>
2025-02-16 22:43:48 +0900

    feat: develop logic of Products in the Busket

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -3,19 +3,29 @@ import { Route, Switch, useRouteMatch } from "react-router-dom";
 import { Container } from "@mui/material";
 import ChosenProduct from "./ChosenProduct";
 import Products from "./Products";
-import "../../../css/products.css"
+import "../../../css/products.css";
+import { CartItem } from "../../../lib/types/search";
 
-export default function ProductPage() {
+interface ProductPageProps {
+  onAdd: (item: CartItem) => void;
+}
+
+export default function ProductPage(props: ProductPageProps) {
+  const { onAdd } = props;
   const products = useRouteMatch();
+  console.log("products:", products);
   
-  return <div className={"products-page"}>
-    <Switch>
-      <Route path={`${products.path}/:productId`}>
-        <ChosenProduct/>
-      </Route>
-      <Route path={`${products.path}`}>
-        <Products />
-      </Route>
-    </Switch>
-  </div>;
+
+  return (
+    <div className={"products-page"}>
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          <ChosenProduct onAdd={onAdd} />
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products onAdd={onAdd} />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
