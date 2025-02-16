7ebe7344ae0adb8fb0ffcb305eff3692761c5402
Thomas <zoirovjohn@gmail.com>
2025-02-17 00:26:26 +0900

    feat: create signup process

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -43,6 +43,8 @@ function App() {
           onRemove={onRemove}
           onDelete={onDelete}
           onDeleteAll={onDeleteAll}
+          setSignupOpen={setSignupOpen}
+          setLoginOpen={setLoginOpen}
         />
       ) : (
         <OtherNavbar
@@ -51,6 +53,8 @@ function App() {
           onRemove={onRemove}
           onDelete={onDelete}
           onDeleteAll={onDeleteAll}
+          setSignupOpen={setSignupOpen}
+          setLoginOpen={setLoginOpen}
         />
       )}
       <Switch>
